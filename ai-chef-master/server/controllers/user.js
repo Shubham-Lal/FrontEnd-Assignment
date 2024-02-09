d62cd2db3655869dbc2bf9fb8cf933ac28918006
Shubham Lal <im.shubhamlal@gmail.com>
2024-02-09 21:44:29 +0530

    ignore

--- ai-chef-master/server/controllers/user.js
+++ ai-chef-master/server/controllers/user.js
@@ -12,16 +12,16 @@ module.exports.login = async (req, res) => {
         }
 
         const user = await User.findOne({ username: username.trim() });
-        if (!user) return res.status(400).json({ success: false, msg: "User doesn't exists!" });
+        if (!user) return res.status(400).json({ success: false, msg: "User doesn't exists" });
 
         const verify = await bcrypt.compare(userData.password, user.password);
-        if (!verify) return res.status(400).json({ success: false, msg: "Incorrect password!" });
+        if (!verify) return res.status(400).json({ success: false, msg: "Incorrect password" });
 
         const token = authToken({ id: user._id.toString() }, "1d");
 
         return res.status(200).json({
             success: true,
-            msg: "Login success!",
+            msg: "Login success",
             data: {
                 _id: user._id,
                 username: user.username,
@@ -43,13 +43,13 @@ module.exports.register = async (req, res) => {
             password: password.trim()
         };
 
-        if (!userData.username) return res.status(405).json({ success: false, msg: "Enter your username!" });
-        else if (!userData.password) return res.status(405).json({ success: false, msg: "Enter your password!" });
-        else if (userData.password.length < 8) return res.status(405).json({ success: false, msg: "Password must be atleast 8 characters!" });
+        if (!userData.username) return res.status(405).json({ success: false, msg: "Enter your username" });
+        else if (!userData.password) return res.status(405).json({ success: false, msg: "Enter your password" });
+        else if (userData.password.length < 8) return res.status(405).json({ success: false, msg: "Password must be atleast 8 characters" });
 
 
         const user_exists = await User.findOne({ username: userData.username });
-        if (user_exists) return res.status(405).json({ success: false, msg: "Username already exists!" });
+        if (user_exists) return res.status(405).json({ success: false, msg: "Username already exists" });
 
         const encryptedPassword = await bcrypt.hash(userData.password, 12);
         await new User({
