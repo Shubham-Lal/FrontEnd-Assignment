c5d80fbe8a276da3ac707bd386f9d8dffc7752b6
Shubham Lal <im.shubhamlal@gmail.com>
2024-02-10 23:39:12 +0530

    wip: navbar

--- ai-chef-master/client/src/components/Navbar/index.tsx
+++ ai-chef-master/client/src/components/Navbar/index.tsx
@@ -1,7 +1,60 @@
-export default function Navbar() {
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaCircleChevronDown } from 'react-icons/fa6';
+import './style.css';
+
+const Navbar = () => {
+    const [expand, setExpand] = useState<boolean>(false);
+    const authToken = localStorage.getItem('token');
+
     return (
-        <div>
-            Navbar
+        <div id='navbar'>
+            <div className={`background ${expand && "expand"}`} />
+
+            <div className='navbar__container'>
+                <Link to='/' className='logo__container'>
+                    <img
+                        className='logo'
+                        src="/logo-transparent.png"
+                        alt="logo"
+                    />
+                </Link>
+                <div className='right__container'>
+                    <Link
+                        to='/'
+                        className='home-btn'
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to='/about'
+                        className='about-btn'
+                    >
+                        About
+                    </Link>
+                    <div className='divider' />
+                    <Link
+                        to='/signup'
+                        className='signup-btn'
+                    >
+                        {!authToken && "Get Started"}
+                    </Link>
+                    <Link
+                        to={!!authToken ? "/dashboard" : "/login"}
+                        className='login-dashboard-btn'
+                    >
+                        {!!authToken ? "Dashboard" : "Login"}
+                    </Link>
+                    <button
+                        onClick={() => setExpand(!expand)}
+                        className={`expand-navbar-btn ${expand && "rotate"}`}
+                    >
+                        <FaCircleChevronDown size={30} fill='white' />
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Navbar
--- ai-chef-master/client/src/pages/home/index.tsx
+++ ai-chef-master/client/src/pages/home/index.tsx
@@ -1,5 +1,14 @@
+import './style.css';
+
 export default function HomePage() {
     return (
-        <div>Home Page</div>
+        <div id='home'>
+            <div className='home__background'>
+                <img
+                    src="/background.png"
+                    alt="background"
+                />
+            </div>
+        </div>
     )
 }
